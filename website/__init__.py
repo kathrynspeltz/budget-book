beb3432d157dd57f55a913d93fb0e76de4546cdc
kathrynspeltz <kathrynspeltz@gmail.com>
2023-11-28 15:55:11 -0600

    env updates

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -15,4 +17,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Expense
+    
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -1,19 +1,18 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
+from website import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 # class Budget(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     monthly_budget = db.Column(db.Integer)
 #     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-# class Expense(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(128), nullable=False)
-#     spent = db.Column(db.Integer, nullable=False)
-#     date = db.Column(db.DateTime(timezone=True), default=func.now())
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    spent = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 # class Tag(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -25,9 +24,10 @@
 # )
 
 
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     username = db.Column(db.String(128), unique=True, nullable=False)
-#     password = db.Column(db.String(128), nullable=False)
-#     # user_goal = db.relationship('UserGoal')
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(128))
+    expenses =db.relationship('Expense')
+    # user_goal = db.relationship('UserGoal')
